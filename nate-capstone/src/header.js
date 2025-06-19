ffea9681852297647dc8c7801bda2bd5d0b7d215
Nathan Lee <nathanlee@C02D8T02ML7H.lan>
2025-06-19 17:24:59 -0400

    adding the updated files with perfect responsive design

--- nate-capstone/src/header.js
+++ nate-capstone/src/header.js
@@ -1,15 +1,34 @@
 import React from "react";
+import { Container, Row, Col, Image } from 'react-bootstrap';
 import Nav from './nav.js';
 
 function Header() {
     return (
       <>
-          <header>
-            <img src="/icons_assets/Logo.svg" alt="company logo"/>
-          <Nav />
+          <header className="py-3" style={{ backgroundColor: '#112240' }}>
+            <Container>
+              <Row className="align-items-center justify-content-center">
+                <Col lg={12} className="p-0">
+                  <Row className='align-items-center justify-content-center g-0'>
+                    <Col col="auto" md="auto" lg={2} className="d-flex justify-content-center">
+                    <Image
+                      src="/icons_assets/Logo.svg"
+                      alt="company logo"
+                      style= {{ maxHeight: '50px' }}
+                      fluid
+                    />
+                    </Col>
+                    <Col lg={2} className="d-none d-lg-block"></Col>
+                    <Col col="auto" md="auto" lg={7} className="d-flex justify-content-center">
+                      <Nav />
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+            </Container>
           </header>
       </>
     );
   }
-  
+
   export default Header;
--- nate-capstone/src/nav.js
+++ nate-capstone/src/nav.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { Nav as BootstrapNav, NavLink } from 'react-bootstrap';
 
 function Nav() {
     return (
-      <>
-          <nav></nav>
-      </>
+      <BootstrapNav className="d-flex justify-content-center">
+        <NavLink href="#home">Home</NavLink>
+        <NavLink href="#about">About</NavLink>
+        <NavLink href="#menu">Menu</NavLink>
+        <NavLink href="#reservations">Reservations</NavLink>
+        <NavLink href="#order">Order Online</NavLink>
+        <NavLink href="#login">Login</NavLink>
+      </BootstrapNav>
     );
   }
-  
+
   export default Nav;
