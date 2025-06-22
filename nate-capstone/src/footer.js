89564e41bf2583dad66efbd4d30d598d9973a001
Nathan Lee <nclee227@gmail.com>
2025-06-22 14:41:41 -0400

    adding updates

--- nate-capstone/src/footer.js
+++ nate-capstone/src/footer.js
@@ -5,18 +5,20 @@ function Footer() {
     return (
       <footer className="mt-auto py-3" style={{backgroundColor: "#edefee"}}>
         <Container>
-          <Row>
+          <Row className="align-items-center justify-content-center">
             <Col lg={1}></Col>
-            <Col lg={4} content-align-center>
+
+            <Col lg={4} md={12} xs={12} className="d-flex justify-content-center footer-image-col">
               <Image
                 src="/icons_assets/Mario and Adrian A.jpg"
                 alt="company logo"
-                style= {{ maxHeight: '100px' }}
+                style= {{ maxHeight: '150px' }}
                 fluid
               />
             </Col>
-            <Col lg={3}>
-              <BootstrapNav className="d-flex flex-column">
+
+            <Col lg={3} md={6} xs={12} className="footer-nav">
+              <BootstrapNav className="nav d-flex flex-column gap-0">
                 <NavLink href="#home" className="nav-item">Home</NavLink>
                 <NavLink href="#about" className="nav-item">About</NavLink>
                 <NavLink href="#menu" className="nav-item">Menu</NavLink>
@@ -25,14 +27,47 @@ function Footer() {
                 <NavLink href="#login" className="nav-item">Login</NavLink>
               </BootstrapNav>
             </Col>
-            <Col lg={3}>
+
+            <Col lg={3} md={6} xs={12} className="footer-contact justify-content-center">
               <Row>
-                <Col xs={12}>Contact info here</Col>
+                <Col xs={12}>
+                  <div>Little Lemon Restaurant</div>
+                  <div>1 Main Street</div>
+                  <div>New York, NY 10010</div>
+                  <div>142-234-6643</div>
+                </Col>
               </Row>
-              <Row>
-                <Col xs={12}>Social links here</Col>
+              <Row className="footer-social mt-3">
+                <Col xs={12} className="text-center text-lg-start">
+                  <a href="https://facebook.com" className="me-3 d-inline-block">
+                    <img
+                      src="/social/facebook-f-brands.svg"
+                      alt="Facebook"
+                      width="30"
+                      height="30" 
+                      style={{filter: 'brightness(0)'}}
+                    />
+                  </a>
+                  <a href="https://instagram.com" className="me-3 d-inline-block">
+                    <img src="/social/square-instagram-brands.svg"
+                      alt="Instagram"
+                      width="30"
+                      height="30"
+                      style={{filter: 'brightness(0)'}}
+                    />
+                  </a>
+                  <a href="https://twitter.com" className="me-3 d-inline-block">
+                    <img src="/social/square-twitter-brands.svg"
+                      alt="Twitter"
+                      width="30"
+                      height="30"
+                      style={{filter: 'brightness(0)'}}
+                    />
+                  </a>
+                </Col>
               </Row>
             </Col>
+
             <Col lg={1}></Col>
           </Row>
         </Container>
