3283b14df76c7aa0820dfb767d7a8df018c0cce8
Nathan Lee <nclee227@gmail.com>
2025-07-05 16:18:08 -0400

    adding final touches

--- nate-capstone/src/App.js
+++ nate-capstone/src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="d-flex flex-column min-vh-100">
         <Header setCurrentScreen={setCurrentScreen} currentScreen={currentScreen} />
-        <Main currentScreen={currentScreen} setCurrentScreen={setCurrentScreen}/>
+        <Main setCurrentScreen={setCurrentScreen} currentScreen={currentScreen}/>
         <Footer setCurrentScreen={setCurrentScreen} currentScreen={currentScreen}/>
     </div>
   );
